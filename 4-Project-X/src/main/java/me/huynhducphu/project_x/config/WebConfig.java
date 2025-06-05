4c731b4aa5234a9796f50140c433d348ab6ebf79
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-06-05 17:30:00 +0700

    updated

--- 4-Project-X/src/main/java/me/huynhducphu/project_x/config/WebConfig.java
+++ 4-Project-X/src/main/java/me/huynhducphu/project_x/config/WebConfig.java
@@ -1,18 +1,20 @@
 package me.huynhducphu.project_x.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Admin 6/5/2025
  **/
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
