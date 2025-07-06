51e915d8959ab7e0fdfa78e29a10d544f4815471
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-07-06 16:48:27 +0700

    handle user can view their uploaded cv: > Add dto for display > Refactor name some dto response class > Add endpoint for update resume file > Add endpoint for remove resume > Add endpoint for get all resume by user id

--- 5-Project-Y-TalentBridge/src/main/java/me/huynhducphu/talent_bridge/controller/ResumeController.java
+++ 5-Project-Y-TalentBridge/src/main/java/me/huynhducphu/talent_bridge/controller/ResumeController.java
@@ -1,15 +1,23 @@
 package me.huynhducphu.talent_bridge.controller;
 
+import com.turkraft.springfilter.boot.Filter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.huynhducphu.talent_bridge.annotation.ApiMessage;
 import me.huynhducphu.talent_bridge.dto.request.resume.ResumeRequestDto;
-import me.huynhducphu.talent_bridge.dto.response.ApiResponse;
+import me.huynhducphu.talent_bridge.dto.response.PageResponseDto;
+import me.huynhducphu.talent_bridge.dto.response.resume.ResumeForDisplayResponseDto;
+import me.huynhducphu.talent_bridge.model.Resume;
 import me.huynhducphu.talent_bridge.service.ResumeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * Admin 7/3/2025
@@ -30,6 +38,42 @@ public class ResumeController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(resumeService.saveResume(resumeRequestDto, pdfFile));
+    }
+
+    @GetMapping("/user/{userId}")
+    @ApiMessage(value = "Lấy resume theo người dùng")
+    public ResponseEntity<?> findResumesByUserId(
+            @PathVariable Long userId,
+            @Filter Specification<Resume> spec,
+            @PageableDefault(size = 5) Pageable pageable) {
+        Page<ResumeForDisplayResponseDto> page = resumeService.findResumesByUserId(userId, spec, pageable);
+
+        PageResponseDto<ResumeForDisplayResponseDto> res = new PageResponseDto<>(
+                page.getContent(),
+                pageable.getPageNumber() + 1,
+                pageable.getPageSize(),
+                page.getTotalElements(),
+                page.getTotalPages()
+        );
+
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping("/users/{userId}/jobs/{jobId}")
+    @ApiMessage(value = "Xóa resume theo user id và job id")
+    public ResponseEntity<?> removeResumeByUserIdAndJobId(
+            @PathVariable Long userId,
+            @PathVariable Long jobId
+    ) {
+        return ResponseEntity.ok(resumeService.removeResumeByUserIdAndJobId(userId, jobId));
+    }
+
+    @PutMapping("/file/{id}")
+    @ApiMessage(value = "Cập nhật file resume")
+    public ResponseEntity<?> updateResumeFile(
+            @PathVariable Long id,
+            @RequestPart("pdfFile") MultipartFile pdfFile) {
+        return ResponseEntity.ok(resumeService.updateResumeFile(id, pdfFile));
     }
 
     @GetMapping("/file/{id}")
--- 5-Project-Y-TalentBridge/src/main/java/me/huynhducphu/talent_bridge/dto/response/skill/DefaultSkillResponseDto.java
+++ 5-Project-Y-TalentBridge/src/main/java/me/huynhducphu/talent_bridge/dto/response/skill/DefaultSkillResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SkillResponseDto {
+public class DefaultSkillResponseDto {
 
     private Long id;
     private String name;
