472a4e973551933e4ecd919c75a112df4cfa1c1c
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-07-15 00:12:28 +0700

    remove old projects

--- 4-Project-X-Frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; 2025</p>
-    </footer>
-  );
-}
